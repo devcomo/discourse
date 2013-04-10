f1f4578b3fc384d84a9226df9c28cb19521ea3f9
Clint Shryock <clint@ctshryock.com>
2013-04-10 11:07:57 -0500

    more tweaking

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-sidekiq_redis = { url: $redis.url, namespace: 'sidekiq', :size => 3 }
+sidekiq_redis = { url: $redis.url, namespace: 'sidekiq', :size => 1 }
 
 Sidekiq.configure_server { |config| config.redis = sidekiq_redis }
 Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
