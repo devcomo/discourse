232336cde9b380c15349363ce8df686bbdfd48e2
Clint Shryock <clint@ctshryock.com>
2013-04-11 11:00:54 -0500

    More sidekiq shit

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-sidekiq_redis = { url: $redis.url, namespace: 'sidekiq', :size => 5 }
+sidekiq_redis = { url: $redis.url, namespace: 'sidekiq', :size => 3 }
 
 Sidekiq.configure_server { |config| config.redis = sidekiq_redis }
 Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
